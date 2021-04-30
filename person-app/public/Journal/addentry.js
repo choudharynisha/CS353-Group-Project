c4c76095eda0d269309565cfd15ea7266ec16463
Nisha Choudhary <choudharynisha024@gmail.com>
2021-04-30 15:33:28 -0400

    Stores Entry as a JSON Object

--- person-app/public/Journal/addentry.js
+++ person-app/public/Journal/addentry.js
@@ -1,3 +1,15 @@
-function submit() {
-    window.open('/public/Journal/viewjournal.html', '_self');
-} // submit()
+function addEntry() {
+    // getting and saving the user's entry
+    let form = new FormData(document.getElementById('entryForm'));
+    
+    for(object of form) {
+        // object = two indices of "entry" and whatever the inserted object was
+        let input = JSON.stringify(object[1]);
+        let today = JSON.stringify(new Date());
+        let entry = '{ "userID":"1234", ' +
+            '"date":' + today + ', ' +
+            '"journalEntry":' + input + '}';
+        entry = JSON.parse(entry);
+        console.log(entry);
+    }
+} // addEntry()
